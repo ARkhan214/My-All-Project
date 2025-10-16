37dcec4bf597435472605a137166ae3f16379bf3
ARkhan214 <abdurrahimkhan214dc@gmail.com>
2025-10-16 23:04:03 +0600

    MK Bank Spring Boot

---
+++ Spring Boot(Backend)/mkbankspring/src/main/java/com/emranhss/mkbankspring/MkBankSpringApplication.java
@@ -0,0 +1,13 @@
+package com.emranhss.mkbankspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkBankSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkBankSpringApplication.class, args);
+	}
+
+}
